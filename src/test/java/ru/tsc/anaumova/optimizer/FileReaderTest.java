8881df87c9a6281d4249da3ddb31ce1aa10d3523
Anastasia Naumova <asyanaumova96@gmail.com>
2021-06-23 12:28:31 +0300

    Тесты.

--- src/test/java/ru/tsc/anaumova/optimizer/FileReaderTest.java
+++ src/test/java/ru/tsc/anaumova/optimizer/FileReaderTest.java
@@ -1,11 +1,14 @@
 package ru.tsc.anaumova.optimizer;
 
+import com.google.gson.reflect.TypeToken;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ru.tsc.anaumova.optimizer.component.FileReader;
 import ru.tsc.anaumova.optimizer.model.Item;
+
+import java.util.List;
 
 public class FileReaderTest {
 
@@ -18,12 +21,17 @@ public class FileReaderTest {
     }
 
     @Test
-    public void readJsonFromFileTest(){
+    public void readFromJsonTest(){
         String jsonString = fileReader.readJsonFromFile();
         Assert.assertFalse(jsonString.isEmpty());
         Assert.assertTrue(jsonString.contains("item - 1"));
         Assert.assertTrue(jsonString.contains("item - 2"));
         Assert.assertTrue(jsonString.contains("item - 3"));
+
+        List<Item> result = fileReader.readFromJSON(new TypeToken<List<Item>>(){}.getType());
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.get(0));
+        Assert.assertEquals(3, result.size());
     }
 
 }
