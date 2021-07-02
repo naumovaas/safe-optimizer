2df7a3956a7cbfd9bc7d6a35c59606b0b203830d
Anastasia Naumova <asyanaumova96@gmail.com>
2021-07-02 14:06:22 +0300

    Правки.

--- src/main/java/ru/tsc/anaumova/optimizer/component/Bootstrap.java
+++ src/main/java/ru/tsc/anaumova/optimizer/component/Bootstrap.java
@@ -1,5 +1,6 @@
 package ru.tsc.anaumova.optimizer.component;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import ru.tsc.anaumova.optimizer.model.Item;
 import ru.tsc.anaumova.optimizer.model.Safe;
@@ -9,21 +10,17 @@ import java.util.List;
 @Component
 public class Bootstrap {
 
-    private MatrixCalculator matrixCalculator;
-
-    private Optimizer optimizer;
-
     public List<Item> startOptimize(final List<Item> items, final Safe safe){
-        matrixCalculator = new MatrixCalculator(items, safe);
-        optimizer = new Optimizer(items, safe);
-        optimizeSafe();
+        MatrixCalculator matrixCalculator = new MatrixCalculator(items, safe);
+        Optimizer optimizer = new Optimizer(items, safe);
+        optimizeSafe(matrixCalculator, optimizer);
         return safe.getItems();
     }
 
     /**
      * Методом динамического программирования определяет набор предметов, имеющих наибольшую стоимость, и помещает их в сейф.
      */
-    public void optimizeSafe(){
+    public void optimizeSafe(@NotNull MatrixCalculator matrixCalculator, @NotNull Optimizer optimizer){
         int[][] matrix = matrixCalculator.calc();
         optimizer.findResult(matrix);
     }
