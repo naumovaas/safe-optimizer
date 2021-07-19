eeb974a9d936e0d23273fd3caaba00ccf7eae338
Anastasia Naumova <asyanaumova96@gmail.com>
2021-07-19 09:26:02 +0300

    Правки.

--- src/main/java/ru/tsc/anaumova/optimizer/Application.java
+++ src/main/java/ru/tsc/anaumova/optimizer/Application.java
@@ -2,10 +2,6 @@ package ru.tsc.anaumova.optimizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.tsc.anaumova.optimizer.component.Bootstrap;
-import ru.tsc.anaumova.optimizer.config.AppConfig;
 
 @SpringBootApplication
 public class Application {
